b8131d4afa606297763c218560a2cb941918f3c7
ThomasRules2000 <tstacey2000@gmail.com>
2016-07-20 17:00:03 +0100

    Updated ocDemoComp to correct ratio

--- ocDemoComp/ocDemoComp.lua
+++ ocDemoComp/ocDemoComp.lua
@@ -12,12 +12,13 @@ local function lines(str)
   return t
 end
 
-local x,y = 108,72
+local x,y = 150,50
 
 gpu.setResolution(x,y) --160x50 = 5x4
 gpu.setForeground(0xffffff,false)
 gpu.setBackground(0x000000,false)
 
+--[==[
 local titleText = {
 [[   ____                    _____                            _                 ]],
 [[  / __ \                  / ____|                          | |                ]],
@@ -39,90 +40,95 @@ local titleText2 = {
 [[                                                       | |                                  ]],
 [[                                                       |_|                                  ]]
 }
+--]==]
+
+local titleText = {
+[[____ ___  ____ _  _ ____ ____ _  _ ___  _  _ ___ ____ ____ ____    ___  ____ _  _ ____    ____ ____ _  _ ___  ____ ___ _ ___ _ ____ _  _ ]],
+[[|  | |__] |___ |\ | |    |  | |\/| |__] |  |  |  |___ |__/ [__     |  \ |___ |\/| |  |    |    |  | |\/| |__] |___  |  |  |  | |  | |\ | ]],
+[[|__| |    |___ | \| |___ |__| |  | |    |__|  |  |___ |  \ ___]    |__/ |___ |  | |__|    |___ |__| |  | |    |___  |  |  |  | |__| | \| ]],
+[[                                                                                                                                         ]],
+}
 
 local titleTextLen = {}
 for i=1,#titleText do
-	titleTextLen[i] = #titleText[i]
+  titleTextLen[i] = #titleText[i]
 end
 term.clear()
 for i = 1,#titleText do
-	term.setCursor(x/2-math.max(table.unpack(titleTextLen))/2,i)
-	term.write(titleText[i])
+  term.setCursor(x/2-math.max(table.unpack(titleTextLen))/2,i)
+  term.write(titleText[i])
 end
 
+--[[
 local titleTextLen2 = {}
 for i=1,#titleText2 do
-	titleTextLen2[i] = #titleText2[i]
+  titleTextLen2[i] = #titleText2[i]
 end
 for i = 1,#titleText2 do
-	term.setCursor(x/2-math.max(table.unpack(titleTextLen2)/2),#titleText+i)
-	term.write(titleText2[i])
+  term.setCursor(x/2-math.max(table.unpack(titleTextLen2)/2),#titleText+i)
+  term.write(titleText2[i])
 end
+--]]
 
 local infoText = {[[
 The following categories are being considered: Demo, EEPROM, Game, Tool, Artwork and Wild.]],
-[[This is not final, however - categories with few entries may get merged into other ones and new categories
-might be created.
+[[This is not final, however - categories with few entries may get merged into other ones and new categories might be created.
 We will show every demo, however voting will only be allowed in official categories.
 
 Entries are accepted until July 29th. Multiple entries from one person are allowed.
 ]],
 [[For categories Demo, Game, Tool and EEPROM:]],
-[[• As standard, you will get a Tier 3 GPU, 2 megabytes of RAM, a Tier 3 Lua 5.3 CPU and the latest version of
-  OpenOS available at the time. Additional cards/peripherals may be requested - especially for sound -
-  however we do not promise they will qualify outside of Wild (especially if they overpower OpenComputers’s
-  built-in capabilities)!
-• Hint from the organizers: OpenComputers has highly limited audio capabilities, but Computronics provides a
-  Beep Card, Noise Card, Sound Card and Tape Drive!
-• If you use the Tape Drive for audio, generating your music inside OpenComputers is considered good form
-  even if it requires preload time to write to the tape.
-• Any additional requirements, the author(s) and a short comment will be shown on an info panel before the
-  demo is displayed.
+[[• As standard, you will get a Tier 3 GPU, 2 megabytes of RAM, a Tier 3 Lua 5.3 CPU and the latest version of OpenOS available at the time.
+  Additional cards/peripherals may be requested - especially for sound - however we do not promise they will qualify outside of Wild (especially
+  if they overpower OpenComputers’s built-in capabilities)!
+• Hint from the organizers: OpenComputers has highly limited audio capabilities, but Computronics provides a Beep Card, Noise Card, Sound Card and
+  Tape Drive!
+• If you use the Tape Drive for audio, generating your music inside OpenComputers is considered good form even if it requires preload time to write
+  to the tape.
+• Any additional requirements, the author(s) and a short comment will be shown on an info panel before the demo is displayed on stage.
 ]],
 [[For categories Demo, Game, Artwork and EEPROM:]],
 [[  • No wireless/Internet connectivity is allowed - your demos must be self-contained.
 ]],
 [[For categories Demo and EEPROM:]],
-[[  • If your demo requires any user input, please let us know. If your demo requires any custom
-    OSes/preparation, please let us know.
+[[  • If your demo requires any user input, please let us know. If your demo requires any custom OSes/preparation, please let us know.
 
-In the Demo category, your demo must fit on one unit’s worth of an OpenComputers storage medium
-(floppy disk or hard drive). Please note that floppy disk contributions are encouraged as they’re
-cooler - and if we get enough of them, we might make a separate category for them!
+In the Demo category, your demo must fit on one unit’s worth of an OpenComputers storage medium (floppy disk or hard drive). Please note that floppy
+disk contributions are encouraged as they’re cooler - and if we get enough of them, we might make a separate category for them!
 
 In the EEPROM category, your demo must run fully off an OpenComputers EEPROM card and load no external data.
 
-In the Game category, we’re going to do our best to play your game - please provide some sort of
-documentation on how to play the game.
+In the Game category, we’re going to do our best to play your game - please provide some sort of documentation on how to play the game.
 
-In the Tool category, you can show non-game applications for OpenComputers - though this will probably
-involve you either providing some sort of demonstration or showing it off yourself (in which case,
-audio chat is necessary).
+In the Tool category, you can show non-game applications for OpenComputers - though this will probably involve you either providing some sort of
+demonstration or showing it off yourself (in which case, audio chat is necessary).
 
-In the Artwork category, your demo should be in the form of a static display on a Tier 3 GPU, coupled with
-its own Lua-based loader. (Hint - asie made a loader called ctif: https://github.com/ChenThread/ctif)
+In the Artwork category, your demo should be in the form of a static display on a Tier 3 GPU, coupled with its own Lua-based loader. (Hint - asie
+made a loader called ctif: https://github.com/ChenThread/ctif)
 
 For the Wild category - go wild! You can use Robots, Drones, even TIS-3D or redstone! 
 ]],
 [[Note: Your demo can’t bring down the entire server in lag. Vanilla redstone might not be a good idea.
 
-Try to do as much as you can yourself. Please state if you have used any of other people's code (including
-non standard apis), what they do and who wrote them. (you could use a sign or monitor for this)
+Try to do as much as you can yourself. Please state if you have used any of other people's code (including non standard apis), what they do and who
+wrote them. (you could use a sign or monitor for this)
 ]]}
-os.sleep(0.1)
-local pos = #titleText+#titleText2+2
+os.sleep(0.5)
+--local pos = #titleText+#titleText2+2
+local pos = #titleText+2
 for i=1,#infoText do
-	local col = 0xffffff
-	if i%2 ==1 then col = 0xff0000 end
-	gpu.setForeground(col,false)
-	term.setCursor(1,pos)
-	term.write(infoText[i])
-	pos = pos+#lines(infoText[i])
+  local col = 0xffffff
+  if i%2 ==1 then col = 0xff0000 end
+  gpu.setForeground(col,false)
+  term.setCursor(1,pos)
+  term.write(infoText[i])
+  pos = pos+#lines(infoText[i])
 end
 
 while true do
-	_, _, _, code = event.pull("key_down")
-	if code == keyboard.keys.enter then
-		break
-	end
+  _, _, _, code = event.pull("key_down")
+  if code == keyboard.keys.enter then
+    gpu.setForeground(0xffffff,false)
+    break
+  end
 end
